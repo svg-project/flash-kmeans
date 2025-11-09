57e404b0064674731245974bd8ef168a7006ac10
jason-fxz <jason341132@qq.com>
2025-11-09 23:23:54 +0800

    [feat] modify api to support large N
    Signed-off-by: jason-fxz <jason341132@qq.com>

--- examples/testapi.py
+++ examples/testapi.py
@@ -1,11 +1,11 @@
-from flash_kmeans import FlashKMeans, batch_kmeans_Euclid
+from flash_kmeans import FlashKMeans, batch_kmeans_Euclid, kmeans_largeN
 import torch
 import time
 
 
 def test_fit_predict_api(B: int, N: int, K: int, D: int):
     data = torch.randn(B, N, D, device="cuda", dtype=torch.float32)
-    kmeans = FlashKMeans(d=D, k=K, niter=20, verbose=False, seed=42, device=torch.device("cuda:0"), use_triton=False)
+    kmeans = FlashKMeans(d=D, k=K, niter=20, verbose=False, seed=42, device=torch.device("cuda:0"), use_triton=True)
     kmeans.fit_predict(data)
 
     start_time = time.time()
@@ -31,7 +31,7 @@ def test_fit_predict_api(B: int, N: int, K: int, D: int):
 
 def test_fit_predict_api_no_batch(N: int, K: int, D: int):
     data = torch.randn(N, D, device="cuda", dtype=torch.float32)
-    kmeans = FlashKMeans(d=D, k=K, niter=20, verbose=False, seed=42, device=torch.device("cuda:0"), use_triton=False)
+    kmeans = FlashKMeans(d=D, k=K, niter=20, verbose=False, seed=42, device=torch.device("cuda:0"), use_triton=True)
     kmeans.fit_predict(data)
 
     start_time = time.time()
@@ -57,7 +57,7 @@ def test_fit_predict_api_no_batch(N: int, K: int, D: int):
 
 def test_train_predict_api(B: int, N: int, K: int, D: int):
     data = torch.randn(B, N, D, device="cuda", dtype=torch.float32)
-    kmeans = FlashKMeans(d=D, k=K, niter=20, verbose=False, seed=42, device=torch.device("cuda:0"), use_triton=False)
+    kmeans = FlashKMeans(d=D, k=K, niter=20, verbose=False, seed=42, device=torch.device("cuda:0"), use_triton=True)
     kmeans.train(data)
 
     start_time = time.time()
@@ -82,8 +82,45 @@ def test_train_predict_api(B: int, N: int, K: int, D: int):
     except Exception as e:
         print(e)
 
+def test_cpu_largeN():
+    N = 131072*5
+    K = 4096
+    D = 128
+    data = torch.randn(N, D, device="cpu", dtype=torch.float32)
+    kmeans = FlashKMeans(d=D, k=K, niter=20, tol=-1, verbose=False, seed=42, use_triton=True, chunk_size_data_cpu=131072)
+    kmeans.train(data)
+    torch.cuda.synchronize()
+
+    start_time = time.time()
+    # kmeans.train(data)
+    api_labels = kmeans.fit_predict(data)
+    torch.cuda.synchronize()
+    end_time = time.time()
+    api_time = end_time - start_time
+
+    print(f"API time: {api_time:.4f} seconds")
+
+    _ = kmeans_largeN(data, n_clusters=K, max_iters=20, tol=-1)
+    
+    torch.manual_seed(42)
+    torch.cuda.manual_seed_all(42)
+    torch.cuda.synchronize()
+    start_time = time.time()
+    raw_labels, _ = kmeans_largeN(data, n_clusters=K, max_iters=20, tol=-1, BLOCK_N=131072)
+    torch.cuda.synchronize()
+    end_time = time.time()
+    raw_time = end_time - start_time
+
+    print(f"RAW time: {raw_time:.4f} seconds")
+
+    try:
+        torch.testing.assert_close(api_labels, raw_labels)
+    except Exception as e:
+        print(e)
+
 if __name__ == "__main__":
 
-    test_fit_predict_api_no_batch(N=742560, K=1000, D=128)
+    test_cpu_largeN()
+    test_fit_predict_api_no_batch(N=131072*5, K=4096, D=128)
     test_train_predict_api(B=32, N=74256, K=1000, D=128)
     test_fit_predict_api(B=32, N=74256, K=1000, D=128)
--- flash_kmeans/__init__.py
+++ flash_kmeans/__init__.py
@@ -1,14 +1,38 @@
 from .interface import FlashKMeans
+
 try:
-    from .kmeans_triton_impl import batch_kmeans_Euclid, batch_kmeans_Cosine, batch_kmeans_Dot
-    from .centroid_update_triton import triton_centroid_update_euclid, triton_centroid_update_sorted_euclid
+    from .kmeans_triton_impl import (
+        batch_kmeans_Euclid,
+        batch_kmeans_Cosine,
+        batch_kmeans_Dot,
+    )
+    from .centroid_update_triton import (
+        triton_centroid_update_euclid,
+        triton_centroid_update_sorted_euclid,
+    )
+    from .kmeans_large import kmeans_largeN, kmeans_largeN_assign
 except Exception:
-    triton_centroid_update_euclid = None
-    triton_centroid_update_sorted_euclid = None
+    import warnings
     from .torch_fallback import batch_kmeans_Euclid_torch_native
+
+    warnings.warn
+    (
+        "Triton kmeans implementation not found, falling back to torch native implementation.",
+        RuntimeWarning,
+    )
+
+    def no_torch_fallback():
+        raise ImportError(
+            "Triton kmeans implementation not found, and this function has no torch fallback."
+        )
+
+    triton_centroid_update_euclid = no_torch_fallback
+    triton_centroid_update_sorted_euclid = no_torch_fallback
     batch_kmeans_Euclid = batch_kmeans_Euclid_torch_native
-    batch_kmeans_Cosine = None
-    batch_kmeans_Dot = None
+    batch_kmeans_Cosine = no_torch_fallback
+    batch_kmeans_Dot = no_torch_fallback
+    kmeans_largeN_assign = no_torch_fallback
+    kmeans_largeN = no_torch_fallback
 
 __all__ = [
     "batch_kmeans_Euclid",
@@ -17,6 +41,8 @@ __all__ = [
     "triton_centroid_update_euclid",
     "triton_centroid_update_sorted_euclid",
     "FlashKMeans",
+    "kmeans_largeN",
+    "kmeans_largeN_assign",
 ]
 
 __version__ = "0.1.1"
